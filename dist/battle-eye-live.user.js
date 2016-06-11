e4b0160f733cf1aeefb0ac4fc593a39f93e836c6
raideer <idmania1@gmail.com>
2016-06-11 22:21:15 +0300

    Removing grant directive so I can access unsafeWindow

--- dist/battle-eye-live.user.js
+++ dist/battle-eye-live.user.js
@@ -17,10 +17,9 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
 // @homepage    https://github.com/raideer
 // @description LIVE battlefield statistics
 // @include     http*://www.erepublik.com/*/military/battlefield-new/*
-// @version     1.0.0-a
+// @version     1.0.1-a
 // @require     https://cdnjs.cloudflare.com/ajax/libs/handlebars.js/4.0.5/handlebars.min.js
 // @run-at      document-idle
-// @grant       none
 // ==/UserScript==
 
 var DpsHandler = function () {
--- src/meta.js
+++ src/meta.js
@@ -5,8 +5,7 @@
 // @homepage    https://github.com/raideer
 // @description LIVE battlefield statistics
 // @include     http*://www.erepublik.com/*/military/battlefield-new/*
-// @version     1.0.0-a
+// @version     1.0.1-a
 // @require     https://cdnjs.cloudflare.com/ajax/libs/handlebars.js/4.0.5/handlebars.min.js
 // @run-at      document-idle
-// @grant       none
 // ==/UserScript==
