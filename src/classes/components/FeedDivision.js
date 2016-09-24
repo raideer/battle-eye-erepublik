47104cbbf1f256bfed710463ba9155d8f3536226
raideer <idmania1@gmail.com>
2016-09-24 16:50:14 +0300

    Restructured the feed

--- src/classes/components/FeedDivision.js
+++ src/classes/components/FeedDivision.js
@@ -16,14 +16,39 @@ class FeedDivision extends React.Component{
         if(settings.highlightDivision.value && SERVER_DATA.division == this.props.div[0]){
             highlightDivision = true;
         }
+
+        var leftDomination = left.damage * battleEyeLive.leftDetBonus;
+        var rightDomination = right.damage * battleEyeLive.rightDetBonus;
+
+        var leftDmgPerPercent = Math.round(parseFloat(left.damage) / parseFloat(this.getPerc(leftDomination, rightDomination)));
+        var rightDmgPerPercent = Math.round(parseFloat(right.damage) / parseFloat(this.getPerc(rightDomination, leftDomination)))
+        if(battleEyeLive.leftDetBonus == battleEyeLive.rightDetBonus){
+            // rightDmgPerPercent = leftDmgPerPercent;
+        }
+
         return (
             <div>
                 <div className={"bel-col-1-1 text-center bel-title " + (highlightDivision?"bel-highlight-title":"")}>
                     {this.props.div[1]}
                 </div>
-                <div className="bel-col-1-3 text-right">
+
+                <div className="belFeedValue">
                     <ul className="list-unstyled">
-                        <li>
+                        <li className="bel-col-1-3 text-right">
+                            <FeedTextValue a={"1% = " + leftDmgPerPercent.toLocaleString() + " dmg"}/>
+                            <FeedValue green={true} a={this.getPerc(leftDomination, rightDomination)} b={this.getPerc(rightDomination, leftDomination)} highlight={settings.highlightValue.value}  text={"%"}/>
+                        </li>
+                        <li className="bel-col-1-3 text-center">Domination</li>
+                        <li className="bel-col-1-3 text-left">
+                            <FeedValue b={this.getPerc(leftDomination, rightDomination)} a={this.getPerc(rightDomination, leftDomination)} highlight={settings.highlightValue.value} text={"%"}/>
+                            <FeedTextValue a={"1% = " + rightDmgPerPercent.toLocaleString() + " dmg"}/>
+                        </li>
+                    </ul>
+                </div>
+
+                <div className="belFeedValue">
+                    <ul className="list-unstyled">
+                        <li className="bel-col-1-3 text-right">
                             <If test={settings.showKills.value}>
                                 <FeedValue green={true} a={left.hits} b={right.hits} highlight={settings.highlightValue.value} text={"kills"}/>
                             </If>
@@ -34,30 +59,8 @@ class FeedDivision extends React.Component{
 
                             <FeedValue green={true} a={left.damage} b={right.damage} highlight={settings.highlightValue.value}/>
                         </li>
-
-                        <If test={settings.showAverageDamage.value}>
-                            <li>
-                                <FeedValue green={true} a={left.avgHit} b={right.avgHit} highlight={settings.highlightValue.value}/>
-                            </li>
-                        </If>
-
-                        <li>
-                            <FeedValue green={true} a={left.dps} b={right.dps} highlight={settings.highlightValue.value}/>
-                        </li>
-                    </ul>
-                </div>
-                <div className="bel-col-1-3 text-center">
-                    <ul className={"list-unstyled bel-titles " + (highlightDivision?"bel-highlight":"")}>
-                        <li>Total Damage</li>
-                        <If test={settings.showAverageDamage.value}>
-                            <li>Average Damage</li>
-                        </If>
-                        <li>DPS</li>
-                    </ul>
-                </div>
-                <div className="bel-col-1-3 text-left">
-                    <ul className="list-unstyled">
-                        <li>
+                        <li className="bel-col-1-3 text-center">Total Damage</li>
+                        <li className="bel-col-1-3 text-left">
                             <FeedValue a={right.damage} b={left.damage} highlight={settings.highlightValue.value}/>
 
                             <If test={settings.showDamagePerc.value}>
@@ -68,25 +71,49 @@ class FeedDivision extends React.Component{
                                 <FeedValue a={right.hits} b={left.hits} text={"kills"} highlight={settings.highlightValue.value}/>
                             </If>
                         </li>
-                        <If test={settings.showAverageDamage.value}>
-                            <li>
+                    </ul>
+                </div>
+
+                <div className="belFeedValue">
+                    <If test={settings.showAverageDamage.value}>
+                        <ul className="list-unstyled">
+                            <li className="bel-col-1-3 text-right">
+                                <FeedValue green={true} a={left.avgHit} b={right.avgHit} highlight={settings.highlightValue.value}/>
+                            </li>
+                            <li className="bel-col-1-3 text-center">Average damage</li>
+                            <li className="bel-col-1-3 text-left">
                                 <FeedValue a={right.avgHit} b={left.avgHit} highlight={settings.highlightValue.value}/>
                             </li>
-                        </If>
-                        <li>
+                        </ul>
+                    </If>
+                </div>
+
+                <div className="belFeedValue">
+                    <ul className="list-unstyled">
+                        <li className="bel-col-1-3 text-right">
+                            <FeedValue green={true} a={left.dps} b={right.dps} highlight={settings.highlightValue.value}/>
+                        </li>
+                        <li className="bel-col-1-3 text-center">DPS</li>
+                        <li className="bel-col-1-3 text-left">
                             <FeedValue a={right.dps} b={left.dps} highlight={settings.highlightValue.value}/>
                         </li>
                     </ul>
                 </div>
+
                 <div className="bel-col-1-1">
+                    <If test={settings.showDominationBar.value}>
+                        <div className="text-left bel-text-tiny">DOMINATION <span className="color-silver">(<strong>{Math.abs(leftDomination-rightDomination).toLocaleString()} </strong> difference)</span></div>
+                        <FeedProgressBar a={leftDomination} b={rightDomination}/>
+                    </If>
+
                     <If test={settings.showDamageBar.value}>
-                        <div className="text-left bel-text-tiny">DAMAGE <span className="color-silver">(<strong>{Math.abs(left.damage-right.damage).toLocaleString()} </strong> difference)</span></div>
+                        <div className="text-left bel-text-tiny">DAMAGE <span className="color-silver">(<strong>{Math.abs(left.damage-right.damage).toLocaleString()} </strong> difference)</span> <span className="color-silver">(1% ~ <strong></strong>)</span></div>
                         <FeedProgressBar a={left.damage} b={right.damage}/>
                     </If>
 
                     <If test={settings.showDpsBar.value}>
-                        <FeedProgressBar a={left.dps} b={right.dps}/>
                         <div className="text-left bel-text-tiny">DPS <span className="color-silver">(<strong>{Math.abs(left.dps-right.dps).toLocaleString()}</strong> difference)</span></div>
+                        <FeedProgressBar a={left.dps} b={right.dps}/>
                     </If>
                 </div>
             </div>
