f814bb5b569a95a4b61d0b83cbde05e93cc30f09
raideer <idmania1@gmail.com>
2016-09-24 16:50:45 +0300

    Component for text values

---
+++ src/classes/components/FeedTextValue.js
@@ -0,0 +1,15 @@
+class FeedTextValue extends React.Component{
+    constructor(){
+        super();
+        this.props = {
+            text: "",
+            green: false
+        };
+    }
+
+    render(){
+        return (
+            <span className="bel-value">{this.props.a} {this.props.text}</span>
+        );
+    }
+}
