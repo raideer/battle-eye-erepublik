f0bccaa8bbbbb8e9fb5974333775b50d6ab7d40f
raideer <idmania1@gmail.com>
2016-09-24 16:51:30 +0300

    Using domination instead of damage

--- src/classes/components/MiniMonitor.js
+++ src/classes/components/MiniMonitor.js
@@ -2,7 +2,7 @@ class MiniMonitor extends React.Component{
 
     getPerc(a, b){
         var ap = 0;
-        if(a+b != 0){
+        if(a+b !== 0){
             ap = Math.round(a * 1000 / (a+b))/10;
         }
 
@@ -13,15 +13,21 @@ class MiniMonitor extends React.Component{
         var data = [];
         var left = this.props.feedData.left;
         var right = this.props.feedData.right;
+
+        var divs = [];
+
         if(SERVER_DATA.division == 11){
-            var divs = [11];
+            divs = [11];
         }else{
-            var divs = [1,2,3,4];
+            divs = [1,2,3,4];
         }
 
         for(var i in divs){
             var div = divs[i];
-            data.push(<div><div className={"bel-div bel-div"+div}></div> {this.getPerc(left.divisions['div'+div].damage, right.divisions['div'+div].damage)}% - {this.getPerc(right.divisions['div'+div].damage, left.divisions['div'+div].damage)}%</div>);
+            var leftDamage = left.divisions['div'+div].damage * battleEyeLive.leftDetBonus;
+            var rightDamage = right.divisions['div'+div].damage * battleEyeLive.rightDetBonus;
+
+            data.push(<div><div className={"bel-div bel-div"+div}></div> {this.getPerc(leftDamage, rightDamage)}% - {this.getPerc(rightDamage, leftDamage)}%</div>);
         }
 
         return data;
@@ -31,7 +37,7 @@ class MiniMonitor extends React.Component{
         if(!settings.showMiniMonitor.value){
             return null;
         }
-        
+
         return (
             <div className="bel-minimonitor">
                 {this.printDivisions()}
