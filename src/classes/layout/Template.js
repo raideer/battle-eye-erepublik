6d96cfee3ea8188b208ed8565306622e3e3d0eba
raideer <idmania1@gmail.com>
2017-11-24 16:27:22 +0200

    Removed footer

--- src/classes/layout/Template.js
+++ src/classes/layout/Template.js
@@ -2,7 +2,6 @@ import SettingsModal from './settings/SettingsModal';
 import TabSelector from './tabs/TabSelector';
 import Tabs from './tabs/Tabs';
 import Header from './Header';
-import Footer from './Footer';
 import React from 'react';
 
 export default class Template extends React.Component {
@@ -47,7 +46,6 @@ export default class Template extends React.Component {
                 <Header openModal={this.openModal.bind(this)} data={this.props.headerData}/>
                 <TabSelector changeTab={this.changeTab.bind(this)} tab={this.state.tab} buttons={this.getTabButtons()} />
                 <Tabs data={this.props.feedData} tab={this.state.tab} />
-                <Footer />
             </div>
         );
     }
