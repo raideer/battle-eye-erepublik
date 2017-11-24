1e11b6debfc6891587e8640ddde5bf0e67f49341
raideer <idmania1@gmail.com>
2017-11-24 21:09:48 +0200

    Meta fix

--- webpack.config.js
+++ webpack.config.js
@@ -44,27 +44,28 @@ module.exports = {
         ]
     },
     plugins: [
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify('production')
+        }),
+        new webpack.optimize.UglifyJsPlugin(),
         new webpack.BannerPlugin({
             banner: `// ==UserScript==
-            // @name        Battle Eye
-            // @namespace   battle-eye-live
-            // @author      Industrials
-            // @homepage    https://docs.google.com/spreadsheets/d/1Ebqp5Hb8KmGvX6X0FXmALO30Fv-IyfJHUGPkjKey8tg
-            // @description Live battle statistics for eRepublik
-            // @include     http*://www.erepublik.com/*/military/battlefield*
-            // @version     ${require('./package.json').version}
-            // @require     https://dl.dropboxusercontent.com/s/tslnlj6lqy63flm/notify.js
-            // @require     https://dl.dropboxusercontent.com/s/7n5isalxf0y6rza/xlsx-populate.js
-            // @run-at      document-idle
-            // @grant       none
-            // @noframes
-            // ==/UserScript==
+// @name        Battle Eye
+// @namespace   battle-eye-live
+// @author      Industrials
+// @homepage    https://docs.google.com/spreadsheets/d/1Ebqp5Hb8KmGvX6X0FXmALO30Fv-IyfJHUGPkjKey8tg
+// @description Live battle statistics for eRepublik
+// @include     http*://www.erepublik.com/*/military/battlefield*
+// @version     ${require('./package.json').version}
+// @require     https://dl.dropboxusercontent.com/s/tslnlj6lqy63flm/notify.js
+// @require     https://dl.dropboxusercontent.com/s/7n5isalxf0y6rza/xlsx-populate.js
+// @run-at      document-idle
+// @grant       none
+// @noframes
+// ==/UserScript==
             `,
             raw: true,
             entryOnly: true
-        }),
-        new webpack.DefinePlugin({
-            'process.env.NODE_ENV': JSON.stringify('production')
         })
     ]
 };
