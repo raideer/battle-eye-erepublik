6f40da84946091fee273d34586f170d583dcb81b
raideer <idmania1@gmail.com>
2018-04-15 14:31:44 +0300

    Minimizing if production; added css loader support

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,32 @@ require('dotenv').config();
 
 const exportPath = process.env.EXPORT_PATH || 'build';
 
+const plugins = [];
+
+if (process.env.NODE_ENV == 'production') {
+    plugins.push(new webpack.optimize.UglifyJsPlugin());
+}
+
+plugins.push(new webpack.BannerPlugin({
+    banner: `// ==UserScript==
+// @name        Battle Eye Live
+// @namespace   battle-eye-live
+// @author      Industrials
+// @homepage    https://docs.google.com/spreadsheets/d/1Ebqp5Hb8KmGvX6X0FXmALO30Fv-IyfJHUGPkjKey8tg
+// @description Live battle statistics for eRepublik
+// @include     http*://www.erepublik.com/*/military/battlefield*
+// @version     ${require('./package.json').version}
+// @require     https://dl.dropboxusercontent.com/s/tslnlj6lqy63flm/notify.js
+// @require     https://dl.dropboxusercontent.com/s/7n5isalxf0y6rza/xlsx-populate.js
+// @run-at      document-idle
+// @grant       none
+// @noframes
+// ==/UserScript==
+    `,
+    raw: true,
+    entryOnly: true
+}));
+
 module.exports = {
     entry: ['babel-polyfill', './src/index.js'],
     output: {
@@ -30,7 +56,7 @@ module.exports = {
                 }
             },
             {
-                test: /\.scss$/,
+                test: /\.(scss|sass)$/,
                 use: [{
                     loader: 'style-loader'
                 }, {
@@ -40,32 +66,18 @@ module.exports = {
                 }, {
                     loader: 'sass-loader'
                 }]
+            },
+            {
+                test: /\.(css)$/,
+                use: [{
+                    loader: 'style-loader'
+                }, {
+                    loader: 'css-loader'
+                }, {
+                    loader: 'postcss-loader'
+                }]
             }
         ]
     },
-    plugins: [
-        new webpack.DefinePlugin({
-            'process.env.NODE_ENV': JSON.stringify('production')
-        }),
-        new webpack.optimize.UglifyJsPlugin(),
-        new webpack.BannerPlugin({
-            banner: `// ==UserScript==
-// @name        Battle Eye Live
-// @namespace   battle-eye-live
-// @author      Industrials
-// @homepage    https://docs.google.com/spreadsheets/d/1Ebqp5Hb8KmGvX6X0FXmALO30Fv-IyfJHUGPkjKey8tg
-// @description Live battle statistics for eRepublik
-// @include     http*://www.erepublik.com/*/military/battlefield*
-// @version     ${require('./package.json').version}
-// @require     https://dl.dropboxusercontent.com/s/tslnlj6lqy63flm/notify.js
-// @require     https://dl.dropboxusercontent.com/s/7n5isalxf0y6rza/xlsx-populate.js
-// @run-at      document-idle
-// @grant       none
-// @noframes
-// ==/UserScript==
-            `,
-            raw: true,
-            entryOnly: true
-        })
-    ]
+    plugins: plugins
 };
